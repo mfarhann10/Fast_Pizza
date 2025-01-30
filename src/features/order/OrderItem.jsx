ab524aa4c5ae66722ddb1b25d2a6f75e05988397
mfarhann10 <mfarhann1009@gmail.com>
2025-01-31 00:10:31 +0700

    add a new components and wrap into file structure

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
