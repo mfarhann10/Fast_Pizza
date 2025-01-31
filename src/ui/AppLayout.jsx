81ac818e0d204510b17443f62076dfe0c49d1743
mfarhann10 <mfarhann1009@gmail.com>
2025-01-31 13:23:02 +0700

    display loader using useNavigation

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,16 +1,26 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
-      <Header />
-      <main>
-        {/* outlet is a nested route */}
-        <Outlet />
-      </main>
-      <CartOverview />
+    <div className="layout">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <main>
+            {/* outlet is a nested route */}
+            <Outlet />
+          </main>
+          <CartOverview />
+        </>
+      )}
     </div>
   );
 }
