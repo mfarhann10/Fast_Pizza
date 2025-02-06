511d89f1035f4a1418830d07b341040495fc730b
mfarhann10 <mfarhann1009@gmail.com>
2025-02-07 01:44:42 +0700

    style the cart

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, onClick, type }) {
   const base =
-    'inline-block rounded-full bg-yellow-400  font-bold uppercase tracking-wide text-stone-800 shadow-md transition hover:bg-yellow-300 disabled:cursor-not-allowed disabled:bg-gray-300';
+    'inline-block rounded-full bg-yellow-400 font-bold uppercase tracking-wide text-stone-800 shadow-md transition hover:bg-yellow-300 disabled:cursor-not-allowed disabled:bg-gray-300';
 
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' py-2 px-4 md:px-5 md:py-2.5 text-xs',
+    secondary:
+      'inline-block bg-stone-200 rounded-full px-4 py-2.5 md:px-6 md:py-3.5 font-bold uppercase tracking-wide text-stone-800 shadow-md transition hover:bg-stone-300 disabled:cursor-not-allowed disabled:bg-gray-300',
   };
   if (to)
     return (
