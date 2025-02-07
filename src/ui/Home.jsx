c0f548db71b9c39411f2606c0f9a2ed5486bf413
mfarhann10 <mfarhann1009@gmail.com>
2025-02-07 19:37:18 +0700

    modeling the redux for user

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser';
+import Button from './Button';
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
     <div className="sm:my16 my-10 px-4 text-center">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -10,7 +13,13 @@ function Home() {
           Straight out of the oven, straight to you.
         </span>
       </h1>
-      <CreateUser />
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button type="primary" to="/menu">
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   );
 }
